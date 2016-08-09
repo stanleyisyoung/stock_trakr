76f8b3db3c7f098fd7fcf381fca40e2e17c29108
FatWalrus <youngsstanley@gmail.com>
2016-08-09 03:01:39 +0000

    add stocks, ui, spinner loading thing

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,15 +1,23 @@
 var init_stock_lookup;
 
 init_stock_lookup = function() {
+  $("#stock-lookup-form").on("ajax:before", function(event, data, status) {
+    show_spinner();
+  });
+  $("#stock-lookup-form").on("ajax:after", function(event, data, status) {
+    hide_spinner();
+  });
+  
   $("#stock-lookup-form").on("ajax:success", function(event, data, status) {
     $("#stock-lookup").replaceWith(data);
     init_stock_lookup(); //need to reinit listeners
   }); //success handling
   
   $("#stock-lookup-form").on("ajax:error", function(even, xhr, status, error) {
+    hide_spinner();
     $("#stock-lookup-results").replaceWith('');
     $("#stock-lookup-errors").replaceWith("Invalid stock.");
-  
+    
   });
   
 };
