5bfa8221b710cd9280f204a02c084c541ed23394
FatWalrus <youngsstanley@gmail.com>
2016-08-10 20:58:44 +0000

    bug fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809034126) do
+ActiveRecord::Schema.define(version: 20160809051102) do
 
   create_table "stocks", force: :cascade do |t|
     t.string   "ticker"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20160809034126) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
